34b629e7f0a8d1e1e49377b27f26613992e66f64
Maria Eduarda T M Oliveira <maria.tmiller2006@gmail.com>
2025-07-02 16:58:03 -0300

    hello word multi linguas

--- hello.py
+++ hello.py
@@ -1,2 +1,33 @@
-print("Hello, World")
+"""
+hello word multi linguas
+dependendo da lingua configurada no ambiente o programa exibe a 
+mensagem corespondente
+
+Como usar:
+tenha a variavel LANG devidadamente configurada ex:
+export LANG=pt_BR
+
+Execução:
+python hello.py   
+ou 
+ ./hello.py
+"""
+__version__ = "0.0.1"
+__author__="Maria Eduarda Oliveira"
+import os
+
+current_language= os.getenv("LANG")[:5]
+
+mensagem = "Hello world!"
+
+if current_language == "pt_BR" :
+    mensagem= "Ola mundo!"
+elif current_language =="it_IT":        
+    mensagem = "Ciao, Mondo!"
+
+
+print(mensagem)
+
+
+
 
