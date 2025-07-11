c9c0d5be14693254d06674ff102e7669d495c7e5
Maria Eduarda T M Oliveira <maria.tmiller2006@gmail.com>
2025-07-11 17:15:12 -0300

    calculadora

--- prefixcalc.py
+++ prefixcalc.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+from datettime import datetime
 arguments = sys.argv[1:]
 
 if not arguments:
@@ -37,5 +40,12 @@ elif operation == "mul" :
     result = n1 * n2
 elif operation == "div" :
     result = n1 / n2
+
+path = os.curdir
+filepath = os.path.join(path, "prefixcal.log")
+timestamp = datetime.now().isoformat()
+
+with open(filepath, "a") as file_:
+    file_.write(f"{timestamp} - {operation},{n1},{n2} = {result}\n")
 
 print ("O resultado e {result}")
